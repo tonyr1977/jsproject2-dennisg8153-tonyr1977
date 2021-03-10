ed833a974595f51de9678dd2615d8458a8a31585
Dennis Grigoryev <dennisg8153@hstat.org>
2021-03-10 23:42:10 +0000

    update repo

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ $.ajax({
     dataType: 'json',
     success: function(data) {
         var stores = [];
-        var borough = prompt("Which borough do you reside in?");
-        var borough2 = borough.substring(0,1).toUpperCase() + borough.substring(1).toLowerCase();
-if (data[i].borough_community == borough2) {
+        var borough = prompt("Which borough do you reside in?"); //Requirement to see which borough the user wants 
+        var borough2 = borough.substring(0,1).toUpperCase() + borough.substring(1).toLowerCase(); // This captializes the first letter.
+        for (var i = 0; i < data.length; i++) {
+if (data[i].borough_community == borough2) { 
     
 }
           
         console.log(data);
+    }
     }
 });
