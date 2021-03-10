afd418539bb5aa7033cae9984923ba40bacded63
Dennis Grigoryev <dennisg8153@hstat.org>
2021-03-10 23:29:14 +0000

    update repo

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $.ajax({
-    url: 'https://api.publicapis.org/entries',
+    url: 'https://data.cityofnewyork.us/resource/35sw-rdxj.json',
     dataType: 'json',
     success: function(data) {
+        var stores = [];
+        var borough = prompt("Which borough do you reside in?");
+        var borough2 = borough.substring(0,1).toUpperCase() + borough.substring(1).toLowerCase();
+if (data[i].borough_community == borough2) {
+    
+}
+          
         console.log(data);
     }
 });
